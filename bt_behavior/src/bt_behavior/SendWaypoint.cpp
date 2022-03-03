3a260638749a6b4f0953c30a88377ae6d7e2de85
bb6698 <b.villalba.2019@alumnos.urjc.es>
2022-03-03 13:47:23 +0100

    Subscriber problems solved

--- bt_behavior/src/bt_behavior/SendWaypoint.cpp
+++ bt_behavior/src/bt_behavior/SendWaypoint.cpp
@@ -17,63 +17,62 @@
 #include <iostream>
 #include <string>
 
+#include "SendWaypoint.hpp"
 #include "behaviortree_cpp_v3/behavior_tree.h"
-#include "nav2_costmap_2d/costmap_2d.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav2_costmap_2d/costmap_2d.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "SendWaypoint.hpp"
 
 namespace bt_behavior {
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
-SendWaypoint::SendWaypoint(const std::string& xml_tag_name, const std::string & action_name, const BT::NodeConfiguration& conf)
+SendWaypoint::SendWaypoint(const std::string& xml_tag_name, const std::string& action_name,
+                           const BT::NodeConfiguration& conf)
     : BT::ActionNodeBase(xml_tag_name, conf)
 
 {
-  data_map_ = node_->create_subscription<nav_msgs::msg::OccupancyGrid>("/map",10,std::bind(&SendWaypoint::MapCallback,node_,_1));
+  data_map_ = node_->create_subscription<nav_msgs::msg::OccupancyGrid>(
+      "/map", 10, std::bind(&SendWaypoint::MapCallback, node_, _1));
 
   config().blackboard->get("node", node_);
 }
 
-void SendWaypoint::halt() {return;}
+void SendWaypoint::halt() { return; }
 
 BT::NodeStatus SendWaypoint::tick() {
-  int counter_visited; //Counter waypoint visited
+  int counter_visited;  // Counter waypoint visited
   if (status() == BT::NodeStatus::IDLE) {
     config().blackboard->get("waypoints", waypoints_);
   }
   config().blackboard->get("visited_wp", counter_visited);
 
   RCLCPP_WARN(node_->get_logger(), "Visitados %d\n", counter_visited);
-  
-  geometry_msgs::msg::PoseStamped waypoint; //Return waypoint
+
+  geometry_msgs::msg::PoseStamped waypoint;  // Return waypoint
   std::vector<double> coords;
-  config().blackboard->get(waypoints_[counter_visited], coords); //Get coords a goal
+  config().blackboard->get(waypoints_[counter_visited], coords);  // Get coords a goal
 
   waypoint.pose.position.x = coords[0];
   waypoint.pose.position.y = coords[1];
 
-  setOutput("goal",waypoint);
+  setOutput("goal", waypoint);
   return BT::NodeStatus::SUCCESS;
 }
 
 }  // namespace bt_behavior
 
 #include "behaviortree_cpp_v3/bt_factory.h"
-//BT_REGISTER_NODES(factory) { factory.registerNodeType<bt_behavior::SendWaypoint>("SendWaypoint"); }
+// BT_REGISTER_NODES(factory) { factory.registerNodeType<bt_behavior::SendWaypoint>("SendWaypoint");
+// }
 
-BT_REGISTER_NODES(factory)
-{
-  BT::NodeBuilder builder =
-    [](const std::string & name, const BT::NodeConfiguration & config)
-    {
-      return std::make_unique<bt_behavior::SendWaypoint>(
-        name, "get_waypoint", config);
-    };
+BT_REGISTER_NODES(factory) {
+  BT::NodeBuilder builder = [](const std::string& name, const BT::NodeConfiguration& config) {
+    return std::make_unique<bt_behavior::SendWaypoint>(name, "get_waypoint", config);
+  };
 
-  factory.registerBuilder<bt_behavior::SendWaypoint>(
-    "SendWaypoint", builder);
+  factory.registerBuilder<bt_behavior::SendWaypoint>("SendWaypoint", builder);
 }
